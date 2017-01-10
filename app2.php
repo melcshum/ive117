a00d4bf1156221eafda485daa3a1bb3a65376383
melcshum <melcshum@gmail.com>
2017-01-10 09:52:50 +0800

    update submitted

--- app2.php
+++ app2.php
@@ -332,7 +332,7 @@
               <div class="col-md-12 col-sm-12 col-xs-12">
                 <div   class="x_panel">
                   <div class="x_title">
-                    <h2>Request Submitted  <small></small></h2>
+                    <h2>Request Submitted</h2>
 
                     <div class="clearfix"></div>
                   </div>
